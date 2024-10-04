dac3182e338185e6f3e04d3f5b145d1ab05595ba
Bohdan Voitov <bohdan.voiyov@gmail.com>
2024-10-04 21:13:32 +0300

    add_task1

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("=== Завдання 1 ===");
+
+// Цикл for
+console.log("Цикл for:");
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// Цикл while
+console.log("Цикл while:");
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+///////////////////////////////////////////////////////////////////////
