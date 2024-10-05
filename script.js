0998272d798780776b6d5034d9771c6c13bbcc22
Bohdan Voitov <bohdan.voiyov@gmail.com>
2024-10-05 14:46:43 +0300

    done_task2

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
 console.log("=== Завдання 1 ===");
 
-// Цикл for
+// for
 console.log("Цикл for:");
 for (let i = 1; i <= 10; i++) {
   console.log(i);
 }
 
-// Цикл while
+// while
 console.log("Цикл while:");
 let i = 1;
 while (i <= 10) {
   console.log(i);
   i++;
 }
+
+console.log("=== Завдання 2 ===");
+
+const mixedArray = [
+  42,
+  "hello",
+  true,
+  3.14,
+  "world",
+  false,
+  null,
+  undefined,
+  100,
+  "JavaScript",
+];
+
+// forEach
+console.log("Метод forEach:");
+mixedArray.forEach((element) => {
+  console.log(typeof element);
+});
+
+// for
+console.log("Циклом for:");
+for (let i = 0; i < mixedArray.length; i++) {
+  console.log(typeof mixedArray[i]);
+}
+
+// while
+console.log("Циклом while:");
+let j = 0;
+while (j < mixedArray.length) {
+  console.log(typeof mixedArray[j]);
+  j++;
+}
+
+// do while
+console.log("Циклом do while:");
+let k = 0;
+do {
+  console.log(typeof mixedArray[k]);
+  k++;
+} while (k < mixedArray.length);
